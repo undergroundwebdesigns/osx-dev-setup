61ab5a5247a8c8ba3fa31d0a2c709a633828f577
Alex Willemsma <alex.willemsma@smile.io>
2023-03-17 07:30:56 -0400

    Add initial setup script and update README.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# osx-dev-setup
-Personal setup scripts to configure a blank OSX machine for development.
+# OSX Development Machine Setup
+
+This repo contains my personal setup scripts for configuring a new Apple Mac computer for use as a development machine, targeting web application development, server and network management, and cloud operations work.
---
+++ setup.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do
+  sudo -n true
+  sleep 60
+  kill -0 "$$" || exit
+done 2>/dev/null &
+
+# Setup Finder Commands
+# Show Library Folder in Finder
+chflags nohidden ~/Library
+
+# Show Hidden Files in Finder
+defaults write com.apple.finder AppleShowAllFiles YES
+
+# Show Path Bar in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show Status Bar in Finder
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Check for Homebrew, and then install it
+if test ! "$(which brew)"; then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo "Homebrew installed successfully"
+else
+  echo "Homebrew already installed!"
+fi
+
+# Install XCode Command Line Tools
+echo 'Checking to see if XCode Command Line Tools are installed...'
+brew config
+
+# Updating Homebrew.
+echo "Updating Homebrew..."
+brew update
+
+# Upgrade any already-installed formulae.
+echo "Upgrading Homebrew..."
+brew upgrade
+
+# Install iTerm2
+echo "Installing iTerm2..."
+brew cask install iterm2
+
+# Update the Terminal
+# Install oh-my-zsh
+if test ! "$(which omz)"; then
+  echo "Installing oh-my-zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "Need to logout now to start the new SHELL..."
+  logout
+else
+  "$ZSH/tools/upgrade.sh"
+fi
+
+# Install Git
+echo "Installing ASDF, Curl, Coreutilities, and Git..."
+brew install asdf coreutils git curl
+
+# Configure zsh plugins:
+sed -n -e '/^plugins=/!p' -e '$aplugins=(asdf git)' ~/.zshrc
+# Reload zsh:
+exec zsh
+
+# Install Powerline fonts
+echo "Installing Powerline fonts..."
+git clone https://github.com/powerline/fonts.git
+cd fonts || exit
+sh -c ./install.sh
+
+# Install a network scanning tool:
+brew install nmap
+
+# Install other useful binaries.
+brew install speedtest_cli
+brew install vim
+
+# Core casks
+brew cask install --appdir="/Applications" alfred
+
+# Misc casks
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" 1password
+brew cask install --appdir="/Applications" caffeine
+
+# Remove outdated versions from the cellar.
+echo "Running brew cleanup..."
+brew cleanup
+echo "You're done!"
